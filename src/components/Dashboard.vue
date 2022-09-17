5b474f3ae27db5e2aa4882f20f149ae0e59b31c5
Renan Maia <renanmaia.rl@gmail.com>
2022-09-17 13:12:36 -0300

    add update status

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -24,9 +24,9 @@
                     </ul>
                 </div>
                 <div>
-                    <select name="status" class="status">
+                    <select name="status" class="status" @change="updatedBurger($event, burger.id)" >
                         <option value="">Selecione</option>                 <!-- binda o status do objeto burger "Solicitado"  -->
-                        <option v-for="s in status" :key="s.id" value="s.tipo" :selected="burger.status == s.tipo" >{{ s.tipo }}</option>
+                        <option v-for="s in status" :key="s.id" :value="s.tipo" :selected="burger.status == s.tipo" >{{ s.tipo }}</option>
                     </select>
                     <button class="delete-btn" @click="deleteBurger(burger.id)">Cancelar</button>
                 </div>
@@ -80,6 +80,22 @@ export default {
             //inserir msg de delete aqui   
 
             this.getPedidos();
+
+        },
+        async updatedBurger(event, id) {
+
+            const option = event.target.value;
+
+            const dataJson = JSON.stringify({ status: option });
+
+            const req = await fetch(`http://localhost:3000/burgers/${id}`, {
+                method: "PATCH",
+                headers: { "Content-Type": "application/json" },
+                body: dataJson
+            });
+
+            const res = await req.json();
+
 
         }
     },
